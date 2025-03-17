178d9ee9e30683872a85b2a9ad5c3fb4ac0dfcc0
Ryan mugei <Lquid1264@gmail.com>
2025-03-17 15:40:52 +0300

    CODE

---
+++ index.js
@@ -0,0 +1,28 @@
+let body = document.body
+let h1=document.getElementById("heading")
+h1.style.backgroundColor="black"
+h1.style.fontSize="50px"
+h1.style.color= "pink"
+
+
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+    { id: 4, name:  "Shawarma", restaurant: "olemukusi"}, image:""
+ ];
+
+ function displayRamens(ramen) {
+    
+} 
+
+function displayRamens(){
+       const ramenMenu = document.getElementById("ramen-menu");
+}
+const form = document.getElementById("form");
+const name = document.getElementById("name")
+const restaurant = document.getElementById("resturant")
+const image = document.getElementById("image")
+
+
+
